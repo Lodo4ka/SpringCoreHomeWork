ca906467b45649d9d6ed0201da343c5e0f69c3f2
Lodo4ka <doomymax@gmail.com>
2017-09-03 21:01:23 +0300

    fourth home work

--- SpringCoreHomeWork3/src/main/java/JavaStreamAPI.java
+++ SpringCoreHomeWork3/src/main/java/JavaStreamAPI.java
@@ -1,29 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.StringJoiner;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class JavaStreamAPI {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         //1
         List<String> list = Arrays.asList("Xenia", "Maxim", "Juliaa");
 
-//        list.stream().map(String::toLowerCase)
-//                .forEach(System.out::println);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> listBuffered = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            String line = bufferedReader.readLine();
+            listBuffered.add(line);
+        }
+
+
+        //1
+        list.stream().map(String::toLowerCase)
+                .forEach(System.out::println);
 
 
 
         //2
 
-//        list.stream().filter(s -> s.length() %2 != 0).forEach(System.out::println);
+        list.stream().filter(s -> s.length() %2 != 0).forEach(System.out::println);
 
         //3 list.stream().map(x -> x).collect(Collectors.joining(" | "));
-        list.stream().map(x -> x).collect(Collectors.joining(" - "));
+        System.out.println(list.stream().collect(Collectors.joining("-")));
+
+        //4
+        long count = listBuffered.stream().filter(s->s.length() > 0).count();
+
+        System.out.println(count);
     }
 
 
